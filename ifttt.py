a621c4189515c1b656a61e2a65696f4b08606dea
corbin <corbinjohnson@me.com>
2019-04-14 11:23:38 -0500

    added timeouts as per best practices.

--- ifttt.py
+++ ifttt.py
@@ -15,11 +15,11 @@ logger.setLevel(logging.INFO)
 
 def fire_ifttt():
     logger.warn("can't contact homerun. Sending notification to IFTTT...")
-    request_to_trigger_ifttt = requests.get(URL_FORMAT % (EVENT, IFTTT_KEY))
+    request_to_trigger_ifttt = requests.get(URL_FORMAT % (EVENT, IFTTT_KEY), timeout=15)
     logger.warn("request to IFTTT sent.  IFTTT request status code: %s" % request_to_trigger_ifttt.status_code)
 
 try:
-    request_for_homerun_online = requests.get(HOMERUN_URL)
+    request_for_homerun_online = requests.get(HOMERUN_URL, timeout=5)
     if request_for_homerun_online.status_code != 200:
         fire_ifttt()
     else:
