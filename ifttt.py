42506672a445bd4033ceda8a1c368dacf0cb0f8d
Corbin Johnson <corbinjohnson@me.com>
2019-04-14 10:50:19 -0500

    initial prototype.

---
+++ ifttt.py
@@ -0,0 +1,28 @@
+import requests
+import logging
+
+IFTTT_KEY  = ""
+URL_FORMAT = "https://maker.ifttt.com/trigger/%s/with/key/%s"
+EVENT      = "homerun_offline"
+HOMERUN_URL= "http://192.168.0.140"
+
+logger = logging.getLogger('homerunpinger')
+handler = logging.FileHandler('/var/tmp/homerunpinger.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+try:
+    request_for_homerun_online = requests.get(HOMERUN_URL+"lflflflflflfl")
+
+
+
+if request_for_homerun_online.status_code != 200:
+    logger.warn("can't contact homerun. Sending notification to IFTTT...")
+    request_to_trigger_ifttt = requests.get(URL_FORMAT % (EVENT, IFTTT_KEY))
+    logger.warn("request to IFTTT sent.  IFTTT request: %s" % request_to_trigger_ifttt.text)
+else:
+    logger.info("Homerun is online.")
+
+def fire_ifttt():
